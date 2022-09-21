5a97731746b1ce98b2eba4d597bab60eb649aa51
jonathon <jonathonwhite@protonmail.com>
2022-09-21 06:19:51 -0400

    move groupbutton full class to groups.cpp

--- src/hci/groups.h
+++ src/hci/groups.h
@@ -10,86 +10,12 @@
 #include "lib/widget/label.h"
 #include "../selection.h"
 
-class GroupButton : public DynamicIntFancyButton
-{
-private:
-	typedef DynamicIntFancyButton BaseWidget;
-	std::shared_ptr<W_LABEL> groupNumberLabel;
-	std::shared_ptr<W_LABEL> groupCountLabel;
-public:
-	size_t groupNumber;
-	static std::shared_ptr<GroupButton> make(size_t groupNumber)
-	{
-		class make_shared_enabler: public GroupButton {};
-		auto widget = std::make_shared<make_shared_enabler>();
-		widget->groupNumber = groupNumber;
-		widget->initialize();
-		return widget;
-	}
-	void initialize()
-	{
-		attach(groupNumberLabel = std::make_shared<W_LABEL>());
-		groupNumberLabel->setGeometry(OBJ_TEXTX, OBJ_B1TEXTY - 5, 16, 16);
-		groupNumberLabel->setString(WzString::fromUtf8(astringf("%u", groupNumber)));
-
-		attach(groupCountLabel = std::make_shared<W_LABEL>());
-		groupCountLabel->setGeometry(OBJ_TEXTX + 40, OBJ_B1TEXTY + 20, 16, 16);
-		groupCountLabel->setString("");
-	}
-	GroupButton() { }
-
-	void clickPrimary() override
-	{
-		// select the group
-		kf_SelectGrouping(groupNumber);
-
-	}
-	void clickSecondary() override
-	{
-		assignDroidsToGroup(selectedPlayer, groupNumber, true);
-
-	}
-protected:
-	void display(int xOffset, int yOffset) override
-	{
-		DROID	*psDroid, *displayDroid = NULL;
-		size_t numberInGroup = 0;
-		std::map<std::vector<uint32_t>, size_t> unitcounter;
-		size_t most_droids_of_same_type_in_group = 0;
-
-		for (psDroid = apsDroidLists[selectedPlayer]; psDroid != nullptr; psDroid = psDroid->psNext) {
-			// display whatever unit occurs the most in this group
-			if (psDroid->group == groupNumber) {
-				// find the identifier for this droid
-				std::vector<uint32_t> components = buildComponentsFromDroid(psDroid);
-				if (++unitcounter[components] > most_droids_of_same_type_in_group) {
-					most_droids_of_same_type_in_group = unitcounter[components];
-					displayDroid = psDroid;
-				}
-				numberInGroup++;
-			}
-		}
-		if (!numberInGroup) {
-			groupCountLabel->setString("");
-			displayBlank(xOffset, yOffset);
-		} else {
-			displayIMD(AtlasImage(), ImdObject::Droid(displayDroid), xOffset, yOffset);
-			groupCountLabel->setString(WzString::fromUtf8(astringf("%u", numberInGroup)));
-		}
-	}
-	std::string getTip() override
-	{
-		return "";
-	}
-	bool isHighlighted() const override
-	{
-		return false;
-	}
-};
 // class GroupController: public BaseObjectsController
 // {
 // 
 // }
+
+class GroupButton;
 
 class GroupsForum: public IntFormAnimated
 {
